adb2b8b6065168d0f47fa61fcda1bffa960c0ad0
Ahana Datta <ahana.datta.ravenclaw@gmail.com>
2021-09-23 22:46:30 +0100

    adding Tanvi's and Chengwei's part, connections working

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
 from app import app
-from flask import render_template
-from server.image_captioning import caption
+from flask import render_template, jsonify, request
+from server.image_captioning import image2caption
+from server.txt_to_emo import preprocess, generate_emo
+import sys
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", title=caption("Imotion"))
+    return render_template("index.html")
+
+@app.route('/img', methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        img = request.form.get("img")
+        print(img)
+    return render_template("index.html")
+
+@app.route('/res', methods=['GET'])
+def txt_to_emo():
+    text = image2caption('./server/img2.jpg')
+    print(text)
+    preds = preprocess(text)
+    response = generate_emo(preds)
+    return jsonify(result=response)
+
